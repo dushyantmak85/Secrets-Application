804b7d2170bd680cdcd7002b56926ecdd5106a42
Dushyant Makwana <dushyantmakwana2004@gmail.com>
2025-04-11 20:59:55 +0530

    Switching to passport.js

--- app.js
+++ app.js
@@ -7,29 +7,33 @@ const encrypt = require('mongoose-encryption');
 const session=require('express-session');
 const passport=require('passport');
 const passportLocalMongoose=require('passport-local-mongoose');
-const app = express();
- 
+const app = express(); 
 //const bcrypt = require("bcrypt");
+
+
 app.use(session({ 
   secret: 'our little secret.',
   resave: false,
   saveUninitialized: false
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://127.0.0.1:27017/myDatabase");
 const UserSchema=new mongoose.Schema({
   email:String,
   password:String
 });
+
 const secret="Thisisourlittlesecret.";
-UserSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
+//UserSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
 
-
+UserSchema.plugin(passportLocalMongoose);
 const User=new mongoose.model("User",UserSchema);
-
-
-
-
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
@@ -47,7 +51,7 @@ app.get("/register", function(req, res){
     res.render("register");
 });
 
-app.post("/register", async function(req, res){
+/*app.post("/register", async function(req, res){
   //const hashedPassword = await bcrypt.hash(req.body.password, 10);
   
   try {
@@ -63,8 +67,34 @@ app.post("/register", async function(req, res){
     res.status(500).send("Error saving user.");
   }
 });
+*/ 
 
-app.post("/login", async function (req, res) {
+//The above code is not longer needed as we are using passport-local-mongoose
+app.post("/register", function(req, res){
+  User.register({username:req.body.username},req.body.password,function(err,user){
+    if(err){
+      console.log(err);
+      res.redirect("/register");
+    }else{
+      passport.authenticate("local")(req,res,function(){
+        res.redirect("/secrets");
+      });
+    }
+  });
+});
+
+app.get("/secrets", function(req, res){
+  if(req.isAuthenticated()){
+    res.render("secrets");
+  }
+  else{
+    res.redirect("/login");
+  }
+}
+);
+
+/*app.post("/login", async function (req, res) {
+
   const username = req.body.username;
   const password = req.body.password;
 
@@ -94,8 +124,29 @@ app.post("/login", async function (req, res) {
     console.error("Login Error:", error);
     res.status(500).send("Internal Server Error");
   }
+});*/
+app.post("/login", function(req, res){
+  const user=new User({
+    username:req.body.username,
+    password:req.body.password
+  });
+  req.login(user,function(err){
+    if(err){
+      console.log(err);
+    }else{
+      passport.authenticate("local")(req,res,function(){
+        res.redirect("/secrets");
+      });
+    }
+  });
 });
 
+app.get("/logout", function(req, res){
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect("/");
+  });
+});
 app.listen(3000, function(){
   console.log("Server started on port 3000");
 });
